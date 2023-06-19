9f109e0601d50467f73619ec506ffa9d4e3062fc
xucl <xucl@300624.cn>
2023-06-19 11:37:54 +0800

    configure ldsoflag += $ORIGIN

--- cmake/configure.cmake
+++ cmake/configure.cmake
@@ -1,29 +1,35 @@
-set(CONFIG_COMMAND "./configure" 
---extra-libs=\"-lpthread -lm\" 
-${IF_ENABLE_FF_SHARED}
---ld=\"g++\" 
---prefix=\"${CMAKE_BINARY_DIR}/ffmpeg_pack\"
-# debug info
-# https://stackoverflow.com/questions/9211163/debugging-ffmpeg
---enable-debug=3 # https://gcc.gnu.org/onlinedocs/gcc/Debugging-Options.html#Debugging-Options
---disable-optimizations
---extra-cflags=-fno-omit-frame-pointer
---extra-cflags=-fno-inline
-# --extra-cflags=-Og
-# --extra-ldsoflags="-Wl,-rpath=\\\$ ORIGIN" # \\$$ORIGIN  
---enable-pic
---enable-gpl 
---enable-gnutls 
---enable-libass 
---enable-libfreetype 
---enable-libmp3lame 
---enable-libopus 
---enable-libvorbis 
---enable-libvpx 
---enable-libx264 
---enable-libx265 
---enable-libdav1d 
-#--enable-libfdk-aac 
-#--enable-nonfree 
-)
+# # the character escaping
+# cmake raw             \\\\\\\$\$ORIGIN
+# ninja                 \\\$$ORIGIN
+# configure option      \\\$ORIGIN
+# make command          \$ORIGIN
+
+
+if(CMAKE_BUILD_TYPE STREQUAL Release)
+else()
+    set(CONFIG_COMMAND "./configure"
+        --extra-libs=\"-lpthread -lm\"
+        ${IF_ENABLE_FF_SHARED}
+        --ld="g++"
+        --prefix="${CMAKE_BINARY_DIR}/ffmpeg_pack"
+        --enable-debug=3 # https://gcc.gnu.org/onlinedocs/gcc/Debugging-Options.html#Debugging-Options
+        --disable-optimizations
+        --extra-cflags=-fno-omit-frame-pointer
+        --extra-cflags=-fno-inline
+        --extra-ldsoflags="-Wl,-rpath=\\\\\\\$\$ORIGIN"
+        --enable-pic
+        --enable-gpl
+        --enable-gnutls
+        --enable-libass
+        --enable-libfreetype
+        --enable-libmp3lame
+        --enable-libopus
+        --enable-libvorbis
+        --enable-libvpx
+        --enable-libx264
+        --enable-libx265
+        --enable-libdav1d
+    )
+endif()
+
 message("CONFIG_COMMAND:${CONFIG_COMMAND}")
